0d8293d72bbcf41de073826f9364c04e9282d76b
andychen5201314 <569267667@qq.com>
2020-07-02 00:06:42 +0800

    Revert "我是注释"
    This reverts commit d0c46adafaac95c1658a357375034171c9e38293.

--- resources/views/layout/errormsg.blade.php
+++
@@ -1,8 +0,0 @@
-@if ($errors->any())
-    @foreach($errors->all() as $error)
-        <li style="color: red">{{ $error }}</li>
-    @endforeach
-@endif
-@if (session()->has('msg'))
-    <li style="color: green">{{ session('msg') }}</li>
-@endif
